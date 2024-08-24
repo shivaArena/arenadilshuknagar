b2966d2a782e483c3eff7ec24081898dcdc2e446
shivaArena <126185455+shivaArena@users.noreply.github.com>
2024-08-24 15:40:05 +0530

    js day01

---
+++ 2 to 4/js/day01.js
@@ -0,0 +1,34 @@
+"use strict"
+//alert("welcome to javascript !")
+
+/*
+multi
+line
+comments
+*/
+
+var person = 5
+var age = 10
+var person = "sriraj"
+
+console.log(person)
+console.log(age)
+
+// es6 in let, const
+let city = "hyderabad"
+city = "ameerpet"
+// let city = "secondrabad" error !
+console.log(city)
+
+// let currentYear = 2024;
+const currentYear = 2024
+// currentYear = 2014 error !
+console.log(currentYear)
+
+let firstname // this a valid 
+let firstNameIs // this the best way 
+// let first name // error
+// let first-name // error
+// let 11firstName // error
+let first1Name // valid
+let first_name // valid but avoid
