40f7198460aafe5efcaa77d954a348aea99526ff
shivaArena <126185455+shivaArena@users.noreply.github.com>
2024-09-10 15:42:41 +0530

    day07

---
+++ 2 to 4/js/day07.js
@@ -0,0 +1,90 @@
+// Number
+// Math Object
+
+let a = 10, b= 5.2, c = 50.5, d = -16;
+// console.log(Math.max(a,b,c))
+// console.log(Math.min(a,b,c))
+// console.log(Math.pow(b,3))
+// console.log(Math.sqrt(a))
+// console.log(Math.tan(a))
+// console.log(Math.sin(a))
+
+// console.log(Math.round(c))
+// console.log(Math.round(b))
+
+//  console.log(Math.floor(b))
+//  console.log(Math.floor(c))
+
+//  console.log(Math.ceil(c))
+//  console.log(Math.ceil(b))
+
+//  console.log(Math.round((Math.random() * 20)))
+//  console.log(Math.abs(d))
+
+// Date Object
+
+/*
+let date = new Date()
+// console.log(date)
+console.log(date.getFullYear())
+console.log(date.getMonth())
+console.log(date.getDay())
+console.log(date.getHours())
+console.log(date.getMinutes())
+
+date.setFullYear(1980)
+
+console.log(date.getFullYear())
+*/
+
+// Arrays Methods
+
+/*
+let subjects = ["html","css","js"]
+let marks = [80,40,50,60,30,subjects]
+let computer = ["mouse","monitor","keyboard"]
+
+console.log(marks)
+console.log(marks[3])
+marks[4] = 100
+console.log(marks)
+console.log(marks[5][2])
+
+console.log(marks.length)
+marks.push("add new element")
+marks.unshift("first")
+
+marks.pop()
+marks.shift()
+marks.sort()
+marks.reverse()
+console.log(marks)
+console.log(subjects.join(","))
+
+console.log(marks.slice(2,5))
+console.log(computer.indexOf("monitor"))
+*/
+
+// window Object
+console.log(window.innerHeight)
+console.log(window.innerWidth)
+// alert("window object")
+
+// let newNum = prompt("Enter a number")
+// console.log(newNum)
+
+// let result = confirm("close this window")
+// console.log(result)
+
+// if(result == true){
+//     window.location = "http://youtube.com"
+// }
+
+// console object
+
+// console.log(console)
+console.info("this is information")
+console.warn("this is warning")
+console.error("error")
+console.log("this is log message")
+console.table()
